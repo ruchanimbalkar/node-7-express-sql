fcca31f7db21e9470c29a934435dd4d54b923ad8
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-12-17 22:08:17 -0800

    fixed issues

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://animals-api-tmkj.onrender.com",
+        target: "https://animals-api-tmkj.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
